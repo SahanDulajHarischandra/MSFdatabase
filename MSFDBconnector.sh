595f782426cc88786d649f6ea532dd7d138cee3f
SahanDulajHarischandra <41647200+SahanDulajHarischandra@users.noreply.github.com>
2018-08-11 14:40:37 +0530

    Create MSFDBconnector.sh

---
+++ MSFDBconnector.sh
@@ -0,0 +1,9 @@
+apt update 
+apt upgrade -y
+cd $HOME/metasploit-framework/config
+curl -LO https://Auxilus.github.io/database.yml
+mkdir -p $PREFIX/var/lib/postgresql
+initdb $PREFIX/var/lib/postgresql
+pg_ctl -D $PREFIX/var/lib/postgresql start
+createuser msf
+createdb msf_database
